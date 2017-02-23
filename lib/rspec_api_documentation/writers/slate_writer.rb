ecbef8aad49e5a799386f1fbb60c9e71c3ba42e7
Nathaniel Watts <reg@nathanielwatts.com>
2017-02-23 15:56:53 -0600

    Adjust slate output & template for MPDX needs

--- lib/rspec_api_documentation/writers/slate_writer.rb
+++ lib/rspec_api_documentation/writers/slate_writer.rb
@@ -20,24 +20,16 @@ module RspecApiDocumentation
 
       def write
         File.open(configuration.docs_dir.join("#{FILENAME}.#{extension}"), 'w+') do |file|
-
-          file.write %Q{---\n}
-          file.write %Q{title: "#{configuration.api_name}"\n}
-          file.write %Q{language_tabs:\n}
-          file.write %Q{  - json: JSON\n}
-          file.write %Q{  - shell: cURL\n}
-          file.write %Q{---\n\n}
+          file.write "# #{configuration.api_name}\n\n"
 
           IndexHelper.sections(index.examples, @configuration).each do |section|
+            sorted_examples = sort_by_stripe_order(section[:examples])
 
-            file.write "# #{section[:resource_name]}\n\n"
-            section[:examples].sort_by!(&:description) unless configuration.keep_source_order
-
-            section[:examples].each do |example|
+            sorted_examples.each do |example|
+            # section[:examples].each do |example|
               markup_example = markup_example_class.new(example, configuration)
               file.write markup_example.render
             end
-
           end
 
         end
@@ -45,6 +37,48 @@ module RspecApiDocumentation
 
       def extension
         EXTENSION
+      end
+
+      private
+
+      def position_from_example_description(description)
+        case
+        when description.start_with?("Create")
+          0
+        when description.start_with?("Retrieve")
+          1
+        when description.start_with?("Update")
+          2
+        when description.start_with?("Delete")
+          3
+        when description.start_with?("List")
+          4
+        when description.start_with?("Bulk create")
+          5
+        when description.start_with?("Bulk update")
+          6
+        when description.start_with?("Bulk delete")
+          7
+        else
+          99
+        end
+      end
+
+      def sort_by_stripe_order(examples)
+        stripe_indexed_list = []
+        alphabetical_list   = []
+
+        examples.each do |example|
+          position = position_from_example_description(example.description)
+
+          if position == 99
+            alphabetical_list << example
+          else
+            stripe_indexed_list[position] = example
+          end
+        end
+
+        stripe_indexed_list.compact + alphabetical_list.sort_by(&:description)
       end
     end
   end
